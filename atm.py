bb5ca9df324215bc849744c8364bba34fd0f9548
navasthi <70795262+navasthi@users.noreply.github.com>
2021-10-15 22:33:30 +0530

    Add files via upload

---
+++ atm.py
@@ -0,0 +1,17 @@
+class Atm:
+    def_init_(self, cardumber, pin):
+        self.cardnumber = cardnumber
+        self.pin = pin
+
+def balanceEnquiry(self):
+ print("The balance left is Rs.250")
+
+ def cashWidthDrawl(self, amount):
+     new amount = 150-amount
+     print(" You have widthdrawed: " + str (amount) + "Your remaining balance is: ") + str(new_amount))
+
+     def mainPart():
+         name = input("Hello, what is your name?")
+         cardnumber = input("Please insert your card number: ")
+         pin = input("Enter your pin: ")
+        
